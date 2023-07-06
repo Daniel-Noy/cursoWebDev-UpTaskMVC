34f09782ea07e2e23d77e37d8db7b9e6bfa2e363
Daniel Noyola <pepenoyola13@gmail.com>
2023-07-05 18:31:02 -0600

    Router para las paginas del login

--- public/index.php
+++ public/index.php
@@ -2,5 +2,31 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
+use MVC\Router;
+$router = new Router();
+
+// Autenticación
+$router->get("/", [LoginController::class, "login"]);
+$router->post("/", [LoginController::class, "login"]);
+
+$router->get("/cuenta/logout", [LoginController::class, "logout"]);
+
+// Cuenta
+$router->get("/cuenta/crear", [LoginController::class, "crear"]);
+$router->post("/cuenta/crear", [LoginController::class, "crear"]);
+
+$router->get("/cuenta/mensaje", [LoginController::class, "crear"]);
+
+$router->get("/cuenta/confirmar", [LoginController::class, "confirmar"]);
+
+// Contraseña
+$router->get("/cuenta/contraseña/olvide", [LoginController::class, "olvide"]);
+$router->post("/cuenta/contraseña/olvide", [LoginController::class, "olvide"]);
+
+$router->get("/cuenta/contraseña/reestablecer", [LoginController::class, "reestablecer"]);
+$router->post("/cuenta/contraseña/reestablecer", [LoginController::class, "reestablecer"]);
+
+
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
